f70b2c0856eb7623766752a2572bc0bbf183daa5
Shane Gladson <shanegladson@uchicago.edu>
2024-08-20 15:10:54 -0400

    Added checks for type of input, not able to cast existing input (#37)
    Co-authored-by: ShaneGladson <shanegladson@gmail.com>

--- src/gradient/gradient.py
+++ src/gradient/gradient.py
@@ -5,8 +5,15 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-@dataclass
+@dataclass(frozen=True)
 class Gradient:
     gradient: NDArray[np.float64]
     variance: Union[NDArray[np.float64], None] = None
     n_samples: Union[int, np.int64, None] = None
+
+    def __post_init__(self) -> None:
+        if not isinstance(self.gradient, np.ndarray):
+            raise TypeError('Gradient must be of type NDArray[np.float64]!')
+
+        if not isinstance(self.variance, np.ndarray):
+            raise TypeError('Variance must be of type NDArray[np.float64]!')
