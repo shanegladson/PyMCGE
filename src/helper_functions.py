9544f53e33244630f8589d928206f2c695ae7e7c
Shane Gladson <shanegladson@uchicago.edu>
2024-08-21 11:53:07 -0400

    Added unit test for get distribution function (#52)

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -2,6 +2,7 @@ from src._distributions._distribution import _Distribution
 from src.distributions import UnivariateNormalDistribution
 from src.distributions import UnivariateUniformDistribution
 from src.distributions import UnivariateDSMaxwellDistribution
+from src.distributions import UnivariateWeibullDistribution
 from src.enums import DistributionType
 
 def get_distribution_from_type(dist_type: DistributionType) -> _Distribution:
@@ -12,5 +13,7 @@ def get_distribution_from_type(dist_type: DistributionType) -> _Distribution:
             return UnivariateNormalDistribution()
         case DistributionType.MAXWELL:
             return UnivariateDSMaxwellDistribution()
+        case DistributionType.WEIBULL:
+            return UnivariateWeibullDistribution()
         case _:
             raise NotImplementedError('Distribution not yet supported!')
---
+++ tests/test_helper_functions.py
@@ -0,0 +1,24 @@
+import unittest
+
+from src.helper_functions import get_distribution_from_type
+from src.enums import DistributionType
+from src.distributions import *
+
+class TestDistributionFromType(unittest.TestCase):
+
+    def test_distribution_from_type(self) -> None:
+        # Uniform distribution 
+        unif_dist = get_distribution_from_type(DistributionType.UNIFORM)
+        self.assertIsInstance(unif_dist, UnivariateUniformDistribution)
+
+        # Uniform distribution 
+        norm_dist = get_distribution_from_type(DistributionType.NORMAL)
+        self.assertIsInstance(norm_dist, UnivariateNormalDistribution)
+
+        # Uniform distribution 
+        maxw_dist = get_distribution_from_type(DistributionType.MAXWELL)
+        self.assertIsInstance(maxw_dist, UnivariateDSMaxwellDistribution)
+
+        # Weibull distribution 
+        weib_dist = get_distribution_from_type(DistributionType.WEIBULL)
+        self.assertIsInstance(weib_dist, UnivariateWeibullDistribution)
