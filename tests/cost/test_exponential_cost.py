e6a1dc360c1aefb8e6eedd0fdcb6bfb2942fb3f0
Shane Gladson <shanegladson@uchicago.edu>
2024-08-23 10:23:01 -0400

    Fixed test names, added cosine cost with offset (#63)

--- tests/cost/test_exponential_cost.py
+++ tests/cost/test_exponential_cost.py
@@ -18,12 +18,12 @@ class TestExponentialCost(unittest.TestCase):
         cls.params1 = np.array([3.0], dtype=np.float64)
         cls.cost = ExponentialCost(cls.params1)
 
-    def test_linear_cost(self) -> None:
+    def test_exponential_cost(self) -> None:
         cost: np.float64 = self.cost.eval_cost(self.x1)
         true_cost: np.float64 = np.float64(np.exp(3.0))
         assert_allclose(cost, true_cost)
 
-    def test_linear_cost_grad(self) -> None:
+    def test_exponential_cost_grad(self) -> None:
         cost_grad: NDArray[np.float64] = self.cost.eval_grad(self.x1)
         true_cost_grad: NDArray[np.float64] = np.array([3.0 * np.exp(3.0)], dtype=np.float64)
         assert_allclose(cost_grad, true_cost_grad)
