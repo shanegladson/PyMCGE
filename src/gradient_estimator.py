dc7958866ef9751d2a2e88f2b1e5f0e174b24ab8
Shane Gladson <shanegladson@uchicago.edu>
2024-08-26 15:42:38 -0400

    Added Final to many member variables (#75)

--- src/gradient_estimator.py
+++ src/gradient_estimator.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Final
 
 from src._costs._cost_function import _CostFunction
 from src._distributions._distribution import _Distribution
@@ -8,9 +9,9 @@ from src.helper_functions import get_distribution_from_type
 
 class GradientEstimator(ABC):
     def __init__(self, cost: _CostFunction, dist_type: DistributionType) -> None:
-        self.__cost: _CostFunction = cost
-        self.__dist_type: DistributionType = dist_type
-        self.__dist: _Distribution = get_distribution_from_type(self.__dist_type)
+        self.__cost: Final[_CostFunction] = cost
+        self.__dist_type: Final[DistributionType] = dist_type
+        self.__dist: Final[_Distribution] = get_distribution_from_type(self.__dist_type)
 
     @property
     def cost(self) -> _CostFunction:
